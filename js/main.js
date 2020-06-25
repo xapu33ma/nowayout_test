3eb4417c7f415dff1af631709f0d12c6063d5054
Misha Kulachynskiy <58039636+xapu33ma@users.noreply.github.com>
2020-06-25 22:26:08 +0300

    добавил таймер на JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let countDownDate = new Date("Jul 1, 2020 00:00:00").getTime();
+// Обновляєм таймер каждую секунду
+let countDownFunction = setInterval(function(){
+    //время на данный момент
+    let now = new Date().getTime();
+    // Вычитываем промежуток времени между установленным временем
+    let distance = countDownDate - now;
+    //высчитываем время для часов,минут,секунд
+    let hours = Math.floor(distance / (1000*60*60));
+    let minutes = Math.floor((distance % (1000*60*60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000*60)) /  1000);
+    //выдаем результат на сайт
+    document.getElementById("timer").innerHTML = hours + ":" + minutes + ":" + seconds;
+    if (distance < 0){
+        clearInterval(countDownFunction);
+        document.getElementById("timer").innerHTML = "Time is out!";
+    }
+}, 1000);
